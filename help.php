ff94c51b01fe8bc38d4c101e19f91d9b272a1ddd
machine <machine@localhost>
2015-09-20 11:01:22 +0200

    page mine inactif ajout des ressources ( metal / cristal /deut + total ) lors du dernier scann modification de la couleur de fond du tableau pour une meilleure visibilit?

--- help.php
+++ help.php
@@ -5,4 +5,9 @@ $help["analyseMI_pt"] = "Nombre de PT utile pour vider 24h de production de cett
 $help["joueur_absent_nbjours"] = "Cette variable correspond aux nombres de jours pendant lequel un joueur n a pas pris de point, il est donc potentiellement absent"; 
 $help["joueur_absent_progression"] = "En r&eacute;glant le nombre de jours &agrave; 0 ou 1 , la progression s apparente &agrave; un top flop journaliers";
 $help["inactif_nbjours"] = "Nombre de jours depuis que ce joueur a ete vu inactif pour la premiere fois";
+$help["analyseMI_ress"] = "Ressources disponibles lors du dernier scan";
+
+
+
+
 ?>
